55f6ae921ee0d888078060c3febe45a74df24953
Jin <jaehwanspin@gmail.com>
2022-02-15 20:59:46 +0900

    intermediate

---
+++ src/events/device_callbacks.h
@@ -0,0 +1,16 @@
+#ifndef __EVENTS_DEVICE_CALLBACKS_H__
+#define __EVENTS_DEVICE_CALLBACKS_H__
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+void uart_irq_callback(const struct device*, void*);
+void lora_recv_callback(const struct device*, uint8_t*, uint16_t, int16_t, int8_t);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif 
